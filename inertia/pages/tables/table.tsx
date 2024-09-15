e4b5b305e99550bcc1d5e792fd656d41c147497a
Tomas Bedrnik <bedrniktom@gmail.com>
2024-09-15 22:03:32 +0200

    add framer animations lol

--- inertia/pages/tables/table.tsx
+++ inertia/pages/tables/table.tsx
@@ -6,6 +6,7 @@ import { formatTime } from '~/utils/time'
 import { CurrentTime } from '~/app/components/current_time'
 import { RouteTypeIcon } from '~/app/components/route_type_icon'
 import { useEffect } from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
 
 export default function Table({
   trips,
@@ -42,72 +43,81 @@ export default function Table({
             )}
           </div>
           <ul className="bg-white dark:bg-slate-900 shadow-lg dark:shadow-none rounded-lg divide-y divide-slate-200 dark:divide-slate-800">
-            {trips.map((trip) => (
-              <li key={trip.tripId} className={`p-6${trip.isWalkable ? '' : ' opacity-50'}`}>
-                <div className="flex items-start space-x-6">
-                  <div className="flex-shrink-0 pt-1">
-                    <div className="bg-primary dark:bg-slate-700 text-primary-foreground w-14 h-14 rounded-full flex items-center justify-center">
-                      <RouteTypeIcon routeType={trip.routeType} className="w-10 h-10" />
+            <AnimatePresence mode="popLayout">
+              {trips.map((trip) => (
+                <motion.li
+                  key={trip.tripId}
+                  layout
+                  className={`p-6${trip.isWalkable ? '' : ' opacity-50'}`}
+                  animate={{ scale: 1, opacity: 1 }}
+                  transition={{ type: 'spring' }}
+                  exit={{ scale: 0.8, opacity: 0 }}
+                >
+                  <div className="flex items-start space-x-6">
+                    <div className="flex-shrink-0 pt-1">
+                      <div className="bg-primary dark:bg-slate-700 text-primary-foreground w-14 h-14 rounded-full flex items-center justify-center">
+                        <RouteTypeIcon routeType={trip.routeType} className="w-10 h-10" />
+                      </div>
                     </div>
-                  </div>
-                  <div className="flex-1 min-w-0">
-                    <div className="flex items-center justify-between">
-                      <div className="flex items-center space-x-2">
-                        <p className="text-2xl font-bold text-primary dark:text-primary-foreground">
-                          {trip.routeName}
+                    <div className="flex-1 min-w-0">
+                      <div className="flex items-center justify-between">
+                        <div className="flex items-center space-x-2">
+                          <p className="text-2xl font-bold text-primary dark:text-primary-foreground">
+                            {trip.routeName}
+                          </p>
+                          {trip.isAirConditioned === true && (
+                            <Snowflake className="w-6 h-6 text-blue-500" />
+                          )}
+                          {trip.isAirConditioned === false && (
+                            <Snowflake className="w-6 h-6 text-slate-300 dark:text-slate-500" />
+                          )}
+                        </div>
+                        <p className="text-2xl font-semibold text-primary dark:text-primary-foreground">
+                          {formatTime(trip.stopTimes.predicted)}
                         </p>
-                        {trip.isAirConditioned === true && (
-                          <Snowflake className="w-6 h-6 text-blue-500" />
-                        )}
-                        {trip.isAirConditioned === false && (
-                          <Snowflake className="w-6 h-6 text-slate-300 dark:text-slate-500" />
+                      </div>
+                      <div className="flex items-center justify-between mt-1">
+                        <p className="text-lg text-slate-600 dark:text-slate-300">
+                          {trip.stopName} → {trip.routeEndStopName}
+                        </p>
+                        {!!trip.stopWalkTime && (
+                          <div
+                            className={`flex items-center justify-end text-sm ${trip.isWalkable ? 'text-slate-600 dark:text-slate-400' : 'text-red-600'}`}
+                          >
+                            <Footprints className="w-4 h-4 mr-1" />
+                            <span>{trip.stopWalkTime} min</span>
+                          </div>
                         )}
                       </div>
-                      <p className="text-2xl font-semibold text-primary dark:text-primary-foreground">
-                        {formatTime(trip.stopTimes.predicted)}
-                      </p>
-                    </div>
-                    <div className="flex items-center justify-between mt-1">
-                      <p className="text-lg text-slate-600 dark:text-slate-300">
-                        {trip.stopName} → {trip.routeEndStopName}
-                      </p>
-                      {!!trip.stopWalkTime && (
-                        <div
-                          className={`flex items-center justify-end text-sm ${trip.isWalkable ? 'text-slate-600 dark:text-slate-400' : 'text-red-600'}`}
-                        >
-                          <Footprints className="w-4 h-4 mr-1" />
-                          <span>{trip.stopWalkTime} min</span>
+                      {trip.lastSeenAtStopName && (
+                        <p className="text-sm text-slate-500 mt-2">
+                          Last seen at: {trip.lastSeenAtStopName}
+                        </p>
+                      )}
+                      {trip.throughStops.length > 0 && (
+                        <div className="mt-4">
+                          <ul className="space-y-2">
+                            {trip.throughStops.map((stop) => (
+                              <li
+                                key={stop.stopId}
+                                className="flex justify-between items-center bg-slate-100 dark:bg-slate-800 rounded-lg p-3"
+                              >
+                                <span className="text-sm font-medium text-slate-700 dark:text-slate-400">
+                                  {stop.stopName}
+                                </span>
+                                <span className="text-sm text-slate-500 dark:text-slate-400">
+                                  {formatTime(stop.stopTimes.predicted)}
+                                </span>
+                              </li>
+                            ))}
+                          </ul>
                         </div>
                       )}
                     </div>
-                    {trip.lastSeenAtStopName && (
-                      <p className="text-sm text-slate-500 mt-2">
-                        Last seen at: {trip.lastSeenAtStopName}
-                      </p>
-                    )}
-                    {trip.throughStops.length > 0 && (
-                      <div className="mt-4">
-                        <ul className="space-y-2">
-                          {trip.throughStops.map((stop) => (
-                            <li
-                              key={stop.stopId}
-                              className="flex justify-between items-center bg-slate-100 dark:bg-slate-800 rounded-lg p-3"
-                            >
-                              <span className="text-sm font-medium text-slate-700 dark:text-slate-400">
-                                {stop.stopName}
-                              </span>
-                              <span className="text-sm text-slate-500 dark:text-slate-400">
-                                {formatTime(stop.stopTimes.predicted)}
-                              </span>
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
-                    )}
                   </div>
-                </div>
-              </li>
-            ))}
+                </motion.li>
+              ))}
+            </AnimatePresence>
           </ul>
         </div>
       </div>
