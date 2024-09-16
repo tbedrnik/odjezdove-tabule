e9e98f06388c1e33165b506cedba7b1339f61dcb
Tomas Bedrnik <bedrniktom@gmail.com>
2024-09-17 01:33:08 +0200

    remove golemio token from env, moved to db.api_keys

--- start/env.ts
+++ start/env.ts
@@ -35,6 +35,4 @@ export default await Env.create(new URL('../', import.meta.url), {
   DB_USER: Env.schema.string(),
   DB_PASSWORD: Env.schema.string.optional(),
   DB_DATABASE: Env.schema.string(),
-
-  GOLEMIO_TOKEN: Env.schema.string(),
 })
